7607ba46640dfd3b9ee18ea637a86d9e17666540
cwj03503 <cwj03503@uga.edu>
2022-03-17 12:51:21 -0400

    update library

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2022 at 10:58 PM
+-- Generation Time: Mar 17, 2022 at 05:50 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -32,13 +32,30 @@ CREATE TABLE `books` (
   `Author` varchar(255) NOT NULL,
   `Publisher` varchar(255) NOT NULL,
   `Genre` varchar(255) NOT NULL,
-  `Year of Publication` date NOT NULL,
+  `YearPubbed` int(4) NOT NULL,
   `Description` text DEFAULT NULL,
-  `ID` int(32) NOT NULL,
+  `BookID` decimal(13,0) NOT NULL,
   `CheckedOut` tinyint(1) NOT NULL DEFAULT 0,
   `Reserved` tinyint(1) NOT NULL DEFAULT 0,
+  `UserID` decimal(10,0) DEFAULT NULL,
   `ImageLocation` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`Title`, `Author`, `Publisher`, `Genre`, `YearPubbed`, `Description`, `BookID`, `CheckedOut`, `Reserved`, `UserID`, `ImageLocation`) VALUES
+('The Odyssey', 'Homer', '', 'Epic', 1614, 'After ten bloody years of fighting in the Trojan War, the intrepid Odysseus heads homeward, little imagining that it will take another ten years of desperate struggle to reclaim his kingdom and family. The wily hero circumvents the wrath of the sea god Poseidon and triumphs over an incredible array of obstacles, assisted by his patron goddess Athene and his own prodigious guile. From a literal descent into Hell to interrogate a dead prophet to a sojourn in the earthly paradise of the Lotus-eaters, the gripping narrative traverses the mythological world of ancient Greece to introduce an unforgettable cast of characters: one-eyed giants known as Cyclopses, the enchantress Circe, cannibals, sirens, the twin perils of Scylla and Charybdis, and a fantastic assortment of other creatures.', '1230057366338', 0, 0, NULL, 'The-Odyssey.jpg'),
+('Dune', 'Frank Herbert', 'Chilton Books', 'Scientific Fiction', 1965, 'Dune is set in the distant future amidst a feudal interstellar society in which various noble houses control planetary fiefs. It tells the story of young Paul Atreides, whose family accepts the stewardship of the planet Arrakis. While the planet is an inhospitable and sparsely populated desert wasteland, it is the only source of melange, or \"spice\", a drug that extends life and enhances mental abilities. Melange is also necessary for space navigation, which requires a kind of multidimensional awareness and foresight that only the drug provides. As melange can only be produced on Arrakis, control of the planet is a coveted and dangerous undertaking.', '1230517172530', 0, 0, NULL, 'Dune.jpg'),
+('The Little Prince', 'Antoine de Saint-Exupéry', 'Reynal & Hitchcock', 'Fantasy', 1943, 'The Sahara desert is the scenery of Little Prince\'s story. The narrator\'s plane has crashed there and he has scarcely some food and water to survive. Trying to comprehend what caused the crash, the Little Prince appears. The serious blonde little boy asks to draw him a sheep. The narrator consents to the strange fellow\'s request. They soon become friends and the Little Prince informs the pilot that he is from a small planet, the asteroid 325, talks to him about the baobabs, his planet volcanoes and the mysterious rose that grew on his planet. He also talks to him about their friendship and the lie that evoked his journey to other planets. Often puzzled by the grown-ups\' behavior, the little traveler becomes a total and eternal symbol of innocence and love, of responsibility and devotion. Through him we get to see how insightful children are and how grown-ups aren\'t. Children use their heart to feel what\'s really important, not the eyes.', '1231296996080', 0, 0, NULL, 'The-Little-Prince.jpg'),
+('Sophie\'s World', 'Jostein Gaarder', 'Aschehoug', 'Realistic Fiction', 1991, 'One day fourteen-year-old Sophie Amundsen comes home from school to find in her mailbox two notes, with one question on each: \"Who are you?\" and \"Where does the world come from?\" From that irresistible beginning, Sophie becomes obsessed with questions that take her far beyond what she knows of her Norwegian village. Through those letters, she enrolls in a kind of correspondence course, covering Socrates to Sartre, with a mysterious philosopher, while receiving letters addressed to another girl. Who is Hilde? And why does her mail keep turning up? To unravel this riddle, Sophie must use the philosophy she is learning—but the truth turns out to be far more complicated than she could have imagined.', '1231691608535', 0, 0, NULL, 'Sophies-World.jpg'),
+('The Shining', 'Stephen King', 'Doubleday', 'Horror', 1977, 'Jack Torrance’s new job at the Overlook Hotel is the perfect chance for a fresh start. As the off-season caretaker at the atmospheric old hotel, he’ll have plenty of time to spend reconnecting with his family and working on his writing. But as the harsh winter weather sets in, the idyllic location feels ever more remote . . . and more sinister. And the only one to notice the strange and terrible forces gathering around the Overlook is Danny Torrance, a uniquely gifted five-year-old.', '1235008127455', 0, 0, NULL, 'The-Shining.jpg'),
+('A Beautiful Mind', 'Sylvia Nasar', 'Simon & Schuster', 'Biography', 1998, 'The powerful, dramatic biography of math genius John Nash, who overcame serious mental illness and schizophrenia to win the Nobel Prize. ', '1235048232108', 0, 0, NULL, 'A-Beautiful-Mind.jpg'),
+('Moby Dick', 'Herman Melville', 'Richard Bentley', 'Epic', 1851, 'Despite strange warnings, Ishmael, a young schoolteacher from Manhattan, signs up for a voyage aboard the Pequod, a whaling ship departing from New Bedford, Massachusetts. While on shore, he strikes up a friendship with Queequeg, a tattooed South Seas cannibal. The unlikely friends are hired for the journey-only to discover their commander will be Captain Ahab, a brooding, one-legged, tyrannical old man fixated on avenging Moby Dick, the great white whale who crippled him.', '1235736443994', 0, 0, NULL, 'Moby-Dick.jpg'),
+('The Fellowship Of The Ring', 'J.R.R. Tolkien', 'George Allen & Unwin', 'Fantasy', 1954, 'In a sleepy village in the Shire, young Frodo Baggins finds himself faced with an immense task, as his elderly cousin Bilbo entrusts the Ring to his care. Frodo must leave his home and make a perilous journey across Middle-earth to the Cracks of Doom, there to destroy the Ring and foil the Dark Lord in his evil purpose.', '1237211382148', 0, 0, NULL, 'The-Fellowship-Of-The-Ring.jpg'),
+('Alan Turing: The Enigma', 'Andrew Hodges', 'Simon & Schuster', 'Biography', 1983, 'It is only a slight exaggeration to say that the British mathematician Alan Turing (1912-1954) saved the Allies from the Nazis, invented the computer and artificial intelligence, and anticipated gay liberation by decades--all before his suicide at age forty-one. This New York Times–bestselling biography of the founder of computer science, with a new preface by the author that addresses Turing\'s royal pardon in 2013, is the definitive account of an extraordinary mind and life.', '1238348070928', 0, 0, NULL, 'Alan-Turing-The-Enigma.jpg'),
+('The Catcher in The Rye', 'J.D. Salinger', 'Little, Brown and Company', 'Realistic Fiction', 1951, 'The hero-narrator of The Catcher in the Rye is an ancient child of sixteen, a native New Yorker named Holden Caulfield. Through circumstances that tend to preclude adult, secondhand description, he leaves his prep school in Pennsylvania and goes underground in New York City for three days. The boy himself is at once too simple and too complex for us to make any final comment about him or his story. Perhaps the safest thing we can say about Holden is that he was born in the world not just strongly attracted to beauty but, almost, hopelessly impaled on it. There are many voices in this novel: children\'s voices, adult voices, underground voices-but Holden\'s voice is the most eloquent of all. Transcending his own vernacular, yet remaining marvelously faithful to it, he issues a perfectly articulated cry of mixed pain and pleasure. However, like most lovers and clowns and poets of the higher orders, he keeps most of the pain to, and for, himself. The pleasure he gives away, or sets aside, with all his heart. It is there for the reader who can handle it to keep.', '1239539868877', 0, 0, NULL, 'The-Catcher-In-The-Rye.jpg');
 
 --
 -- Indexes for dumped tables
@@ -48,17 +65,7 @@ CREATE TABLE `books` (
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `books`
---
-ALTER TABLE `books`
-  MODIFY `ID` int(32) NOT NULL AUTO_INCREMENT;
+  ADD PRIMARY KEY (`BookID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
